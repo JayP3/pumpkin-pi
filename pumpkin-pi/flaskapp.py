81092c7948fadb764db8bf9fb36c33cfd89ff8ef
Jason Pierrepont <dreamssswalker@gmail.com>
2014-10-10 21:13:28 +0000

    Added some functionality to the Blink Lights button

--- pumpkin-pi/flaskapp.py
+++ pumpkin-pi/flaskapp.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-    
+
+@app.route('/blink', methods=['POST'])
+def blink():
+    print "Blinking Light!!!"
+    return app.send_static_file('index.html')
+ 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
