bf0a5b0894e4772545200b042a4e8d536cc3004c
Jason Pierrepont <json6381@aol.com>
2014-10-10 17:50:59 -0400

    Added sounds. Added button to play sounds

--- pumpkin-pi/flaskapp.py
+++ pumpkin-pi/flaskapp.py
@@ -10,6 +10,11 @@ def index():
 def blink():
     print "Blinking Light!!!"
     return app.send_static_file('index.html')
+    
+@app.route('/sound', methods=['POST'])
+def sound():
+    print "Playing Sound"
+    return app.send_static_file('index.html')
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
