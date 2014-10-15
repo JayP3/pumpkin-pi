364556a96c4865443d2c34a653cfb4984e63d268
Jason Pierrepont <dreamssswalker@gmail.com>
2014-10-15 01:55:13 +0000

    cleaned up python code

--- pumpkin-pi/flaskapp.py
+++ pumpkin-pi/flaskapp.py
@@ -1,34 +1,42 @@
-from flask import Flask, render_template
-import blink
-from pumpkinpi import PumpkinPi, pins 
+from flask import Flask, render_template, request
+from pumpkinpi import PumpkinPi, pins
 import RPi.GPIO as GPIO
 
 
 app = Flask(__name__)
-pumpkin = PumpkinPi(GPIO.BCM, pins )
+pumpkin = PumpkinPi(GPIO.BCM, pins)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/blink', methods=['POST'])
 def blink_lights():
     print "Blinking Light!!!"
-    pumpkin.multi_blink([23, 24], 1, 5 )
+    pumpkin.multi_blink([23, 24], 1, 5)
     return render_template('index.html')
-    
+
+
 @app.route('/sound', methods=['POST'])
 def sound():
     print "Playing Sound"
     pumpkin.play_sound()
     return render_template('index.html')
 
+
 @app.route('/watch', methods=['POST'])
 def watch():
-    print "Watching for movement!!!"
-    pumpkin.run()
+    if request.form['value'] == 'Start Watching':
+        print "Watching for movement!!!"
+        pumpkin.run()
+    elif request.form['value'] == 'Stop Watching':
+        print "Stopping the Watch thread"
+        pumpkin.stop()
+
     return render_template('index.html')
- 
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
--- pumpkin-pi/pumpkinpi.py
+++ pumpkin-pi/pumpkinpi.py
@@ -1,8 +1,10 @@
-import RPi.GPIO as GPIO
-import time, random, os
+import time
+import random
+import os
 import threading
+import RPi.GPIO as GPIO
 
-LEFT_LED  = 24
+LEFT_LED = 24
 RIGHT_LED = 23
 PIR_POWER = 17
 PIR_SENSE = 4
@@ -18,7 +20,7 @@ pins = {LEFT_LED:  (GPIO.OUT, 'LED'),
         RIGHT_LED: (GPIO.OUT, 'LED'),
         PIR_POWER: (GPIO.OUT, 'SENSOR_POWER'),
         PIR_SENSE: (GPIO.IN,  'SENSOR')
-}
+        }
 
 
 class PumpkinPi(object):
@@ -26,54 +28,55 @@ class PumpkinPi(object):
     """
     def __init__(self, mode, pins):
         """Initialize a PumpkinGPIO object"""
-        self.loop_thread = None
+        self.kill_thread = threading.Event()
 #        self.gpio = PumpkinGPIO(mode, pins)
         self.mode = mode
         self.pins = pins
         self.setup_pins(mode, pins)
-        
+
     def run(self):
-        t = Watcher(self)
+        self.kill_thread.clear()
+        t = Watcher(self, self.kill_thread)
         t.start()
         self.loop_thread = t
-        
+
     def stop(self):
-        pass
-    
+        print "Killing the Watcher thread"
+        self.kill_thread.set()
+
     def blink_lights(self):
         pass
-    
+
     def multi_blink(self, pins, duration, repeat=1):
         for i in range(0, repeat):
             for pin in pins:
-                 GPIO.output(pin, GPIO.HIGH)
+                GPIO.output(pin, GPIO.HIGH)
             time.sleep(duration)
             for pin in pins:
                 GPIO.output(pin, GPIO.LOW)
-            time.sleep(duration)   
+            time.sleep(duration)
 
     def play_sound(self):
         sounds = os.listdir(os.path.join(os.getcwd(), 'sounds'))
         num = random.randint(0, len(sounds) - 1)
         fname = os.path.join('sounds', sounds[num])
         cmd = 'mpg321 -g 500 %s &' % fname
-        os.system(cmd)    
-   
+        os.system(cmd)
+
     def setup_pins(self, mode, pins):
         """Set up pins for input or output.
-        Resets all pins before anything is set.
         Arguments:
           mode       : Either GPIO.BCM or GPIO.BOARD
           pins (dict): A dictionary of pin numbers and pin setup,
                       (GPIO.IN or GPIO.OUT.
         """
-        GPIO.cleanup()
         GPIO.setmode(mode)
         for pin in pins.keys():
             GPIO.setup(pin, pins[pin][0])
 
     def cleanup_gpio():
-        pass
+        GPIO.cleanup()
+
 
 class PumpkinGPIO(object):
     """This is where all the info regarding what is connected
@@ -88,11 +91,11 @@ class PumpkinGPIO(object):
         BCM number of the pin.
         Arguments:
           mode: GPIO.BCM or GPIO.BOARD. See Python RPi.GPIO for details.
-          pins (dict): Keys = the pin numbers, values = GPIO.IN or GPIO.OUT 
+          pins (dict): Keys = the pin numbers, values = GPIO.IN or GPIO.OUT
         """
         self.mode = mode
         self.pins = pins
-        
+
     def set_output(self, pin, output):
         """For pins set to GPIO.OUT, change their output from low to high.
         """
@@ -104,7 +107,7 @@ class PumpkinGPIO(object):
           pin (int): the number of the GPIO pin
         """
         return GPIO.input(pin)
-        
+
     def setup_pins(self, mode, pins):
         """Set up pins for input or output.
         Resets all pins before anything is set.
@@ -120,26 +123,30 @@ class PumpkinGPIO(object):
 
     def cleanup(self):
         GPIO.cleanup()
-    
+
+
 class Watcher(threading.Thread):
     """Class to start a loop where if motion is sensed, lights will
     blink and sounds will play if speakers are connected.
     Depends on a PumpkinGPIO object.
     """
-    def __init__(self, controller):
+    def __init__(self, controller, stop):
+        threading.Thread.__init__(self)
         self.controller = controller
-        self.stoprequest = threading.Event()
-    
+        self.stoprequest = stop
+
     def run(self):
         time.sleep(10)
+        GPIO.output(PIR_POWER, GPIO.HIGH)
 
-        while 1:
+        while not self.stoprequest.is_set():
             movement = GPIO.input(4)
+
             try:
-                if movement == True:
+                if movement is True:
                     print "I sense movement!!!"
-                    play_sound()
-                    self.controller.multi_blibnk([23, 24], 1, 8) 
+                    self.controller.play_sound()
+                    self.controller.multi_blibnk([23, 24], 1, 8)
                     time.sleep(10)
                 else:
                     print ".",
@@ -151,21 +158,20 @@ class Watcher(threading.Thread):
         GPIO.cleanup()
         exit()
 
-class LED(object):
-    def blink(self):
-        GPIO.output(pin, GPIO.HIGH)
-        
+
 def simple_blink(pin, duration):
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(duration)
     GPIO.output(pin, GPIO.LOW)
     time.sleep(duration)
 
+
 def complex_blink(pin, on_time, off_time):
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(on_time)
     GPIO.output(pin, GPIO.LOW)
     time.sleep(off_time)
+
 
 def repeated_blink(pin, duration, repeat=1):
     i = 0
@@ -185,6 +191,7 @@ def multi_blink(((pins), duration, rest)):
     if rest:
         time.sleep(rest)
 
+
 def multi_blinks(pattern, repeat=1):
     """
     pattern is a list or tuple, where each element is ((pins), duration, rest)
@@ -193,24 +200,20 @@ def multi_blinks(pattern, repeat=1):
         for i in pattern:
             multi_blink(i)
 
+
 rightblnk_M = ((RIGHT_LED,), MEDIUM, 0)
-left_blnk_M =   ([LEFT_LED], MEDIUM, 0)
+left_blnk_M = ([LEFT_LED], MEDIUM, 0)
 rightblnk_F = ((RIGHT_LED,), FAST, 0)
-left_blnk_F =   ([LEFT_LED], FAST, 0)
+left_blnk_F = ([LEFT_LED], FAST, 0)
 dl_blnk_F = ((LEFT_LED, RIGHT_LED), FAST, FAST)
 dl_blnk_S = ((LEFT_LED, RIGHT_LED), SLOW, SLOW)
 
-pattern01 = (
-             rightblnk_M, left_blnk_M, rightblnk_M,
-             left_blnk_M, dl_blnk_F, dl_blnk_F, 
+pattern01 = (rightblnk_M, left_blnk_M, rightblnk_M,
+             left_blnk_M, dl_blnk_F, dl_blnk_F,
              dl_blnk_F, dl_blnk_F
              )
 
 
-pattern02 = (
-            dl_blnk_F, rightblnk_F, left_blnk_F, dl_blnk_F
-            )
-            
-pattern02 = (
-            dl_blnk_F, 
-            )
+pattern02 = (dl_blnk_F, rightblnk_F, left_blnk_F, dl_blnk_F)
+
+pattern02 = (dl_blnk_F,)
